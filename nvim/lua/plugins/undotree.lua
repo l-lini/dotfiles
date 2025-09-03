aaea31ecf40e7c528638569b18a46a2daa52c49c
Lini <119787571+B1nus@users.noreply.github.com>
2025-09-03 21:49:55 +0200

    persistent undotree

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,3 +1,8 @@
 return {
-  "mbbill/undotree",
+	"mbbill/undotree",
+	config = function() 
+		vim.opt.undofile = true
+		vim.opt.undodir = vim.fn.expand("~/.local/share/nvim/undo")
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+	end
 }
