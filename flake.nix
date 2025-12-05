d7c12a880ddfaa2df0dd125954d61bc73f02b5fd
Lini <Lini>
2025-12-06 00:18:46 +0100

    Init structure

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# Git default branch = main
+# Git name = lini
+# Git mail = fuck@of.
+# Remove all dirs except for Documents, Pictures, Projects, Password Digests, ... (ephemeral root)
+# Vim terminal, Vim nmtui, Vim browser (qutebrowser), Vim window manager (maybe not hyprland)
+# Nmtui with fix for eduroam (keep somewhere where ephemeral root won't delete of course)
+# Git and ssh stuffies in some ephemeral root safe dir
+# Nice background
+# Nice consistent color scheme and font and styling
+# Battery-, Network-, Time-, Keybinds-, Sound-, notif on keybind
+# Switch language keybind with notif for language
+# neovim config for root and user:
+  # lsp easy
+  # tree-sitter easy
+  # auto indent detection
+  # harpoon or fuzzy finder or file explorer or similar (pick one)
+  # undo-tree
+# zoxide
+# zsh
+# zfs (disko)
+# cachix (binary builds of packages I think)
+# Home manager of course (match the versions)
+# Window manager
+  # Launcher
+  # Terminal
+  # Browser
+  # File explorer (Vim keybinds)
+  # Fullscreen
+  # Move
+  # Switch
+  # Floating toggle
+  # Resizing
+  # Screenshot
+  # Master slave
+  # Blurred background on not focused. Opaque if focused
+  # toggle mirror screen
+# Slack and Discord
