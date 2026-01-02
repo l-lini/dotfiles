d973b8aabb59cf1d868d912a1fe5867372a3cd05
Lini <Lini>
2026-01-02 01:40:41 +0100

    displaymanager working??

--- flake.nix
+++ flake.nix
@@ -6,15 +6,23 @@
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 		# nixpkgs-instable.url = "github:nixos/nixpkgs/nixos-unstable";
 		nvf.url = "github:notashelf/nvf";
+                swaymonad.url = "github:nicolasavru/swaymonad";
 		home-manager.url = "github:nix-community/home-manager/release-25.11";
 	};
 
-	outputs = inputs @{ nixpkgs, home-manager, nvf, ... }: {
+	outputs = inputs @{ nixpkgs, home-manager, nvf, swaymonad, ... }: let
+                system = "x86_64-linux";
+        in {
 		nixosConfigurations = {
 			lini = nixpkgs.lib.nixosSystem {
 				modules = [
 					./system
 					nvf.nixosModules.default
+                                        ({ ... }: {
+                                                environment.systemPackages = [
+                                                        swaymonad.defaultPackage.${system}
+                                                ];
+                                        })
 					home-manager.nixosModules.home-manager {
 						home-manager.useGlobalPkgs = true;
 						home-manager.useUserPackages = true;
--- system/default.nix
+++ system/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, nvf, ... }:
+{ config, lib, pkgs, ... }:
 
 {
         imports = [
@@ -33,6 +33,7 @@
                 # Flakes clones its dependencies through the git command,
                 # so git must be installed first
                 pavucontrol
+                spotify
                 r2modman
                 anki
                 git
@@ -117,6 +118,48 @@
         services.mysql = {
                 enable = true;
                 package = pkgs.mariadb;
+        };
+
+        services.tlp = {
+                enable = true;
+                settings = {
+                        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+                        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+                        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+                        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+                        CPU_MIN_PERF_ON_AC = 0;
+                        CPU_MAX_PERF_ON_AC = 100;
+                        CPU_MIN_PERF_ON_BAT = 0;
+                        CPU_MAX_PERF_ON_BAT = 20;
+
+                        # Optional helps save long term battery health
+                        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+                        STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+                };
+        };
+
+        services.auto-cpufreq = {
+                enable = true;
+                settings = {
+                        battery = {
+                                governor = "powersave";
+                                turbo = "never";
+                        };
+                        charger = {
+                                governor = "performance";
+                                turbo = "auto";
+                        };
+                };
+        };
+
+        services.displayManager.cosmic-greeter.enable = true;
+
+        services.logind.settings.Login = {
+                HandleLidSwitch = "poweroff";
+                HandleLidSwitchExternalPower = "lock";
+                HandleLidSwitchDocked = "ignore";
         };
 
         i18n.inputMethod = {
