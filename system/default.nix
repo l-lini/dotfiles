0edca38c023279389f1eca343ccab56a2bb4fd0b
Lini <Lini>
2026-01-09 00:11:01 +0100

    nvcat and remove nvf (that error made me go fucking crazy)

--- system/default.nix
+++ system/default.nix
@@ -35,7 +35,7 @@
 
         # You can use https://search.nixos.org/ to find more packages (and options).
         environment.systemPackages = with pkgs; [
-                nvcat # ^^
+                # nvcat # ^^
                 neovim
                 prismlauncher
                 heroic
