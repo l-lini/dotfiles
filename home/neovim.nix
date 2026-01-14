9b85affc3f1f57fd1b9362290180b9c6e0619349
Lini <Lini>
2026-01-14 22:17:54 +0100

    fixed most things neovim?

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
 	programs.neovim = {
@@ -6,58 +6,97 @@
 		defaultEditor = true;
 		viAlias = true;
 		vimAlias = true;
+		extraLuaConfig = let 
+			early = lib.mkOrder 5000 ''
+				vim.schedule(function()
+					vim.o.clipboard = 'unnamedplus'
+				end)
+				vim.o.undofile = true
+				tabstop = 2
+				expandtab = true
+				vim.keymap.set("n", "<C-e>", vim.diagnostic.open_float, {
+					desc = "Show LSP diagnostic under cursor",
+				})
+				vim.api.nvim_create_autocmd("FileType", {
+					callback = function(args)
+						vim.treesitter.start(args.buf)
+					end,
+				})
+
+			'';
+			late = lib.mkAfter "";
+		in
+			lib.mkMerge [ early late ];
 		plugins = with pkgs.vimPlugins; [
+			guess-indent-nvim # Please work. I'm begging, begging youuuuuu
 			{
 				plugin = nvim-lspconfig;
 				type = "lua";
 				config = ''
-					vim.lsp.config('*', {
-						capabilities = {
-							textDocument = {
-								semanticTokens = {
-									multilineTokenSupport = true,
-								}
-							}
-						},
-						root_markers = { '.git' },
-					})
+					vim.lsp.config('*', { root_markers = { '.git' }, })
 
-					vim.lsp.config('hls', {})
-					vim.lsp.config('lua_ls', {})
-					vim.lsp.config('rnix-lsp', {})'';
+                                        local configs = {
+                                                { 'hls', {} },
+                                                { 'lua_ls', {} },
+                                                { 'rnix-lsp', {} },
+                                        }
+
+                                        for _, lsp in pairs(configs) do
+                                                local name, config = lsp[1], lsp[2]
+                                                vim.lsp.enable(name)
+						vim.lsp.config(name, config)
+                                        end
+				'';
 			}
-			nvim-treesitter.withAllGrammars
-			# telescope-nvim
-			# telescope-fzf-native-nvim
-			# harpoon
-			# vim-fugitive
-			# {
-			# 	plugin = nvim-lspconfig;
-			# 	type = "lua";
-			# 	config = "require('lspconfig').hls.setup {};";
-			# }
-			# use coc-nvim to have LSP
+		{
+			plugin = nvim-treesitter.withAllGrammars;
+			type = "lua";
+			config = ''
+				require'nvim-treesitter'.setup {
+					build = ':TSUpdate',
+				      	main = 'nvim-treesitter.configs',
+				      	opts = {
+					      	ensure_installed = 'all',
+					      	auto_install = true,
+					      	highlight = { enable = true },
+					      	indent = { enable = true },
+				      	},
+				}
+			'';
+		}
+# telescope-nvim
+# telescope-fzf-native-nvim
+# harpoon
+# vim-fugitive
+# {
+# 	plugin = nvim-lspconfig;
+# 	type = "lua";
+# 	config = "require('lspconfig').hls.setup {};";
+# }
+# use coc-nvim to have LSP
 		];
 		extraPackages = [];
 	};
-        # All these neovim distros for nix suck as. Just do it the way it's intended.
-        # stop autocomplete in comments. fuck you bitch. that's fucking dumb as hell. 
-        # Default to tabwidth = 2
-        # relative line numbers
-	# relative line numbers in explorer
-	# grep thingie in folder
-        # disable arrow keys
-        # disable mouse (in all terminal instances and neovim etc...) ()
-        # lsp easy
-        # tree-sitter easy
-        # auto indent detection
-        # harpoon or fuzzy finder or file explorer or similar (pick one)
-        # undo-tree
-        # system clipboard
-        # Auto format
-        # Autosave on close
-        # Auto sudo on write protected
-        # haskell with good lsp. hlint builtin.
-        # Se lsp and tree-sitter errors
-        # Autocomplete
+# All these neovim distros for nix suck as. Just do it the way it's intended.
+# stop autocomplete in comments. fuck you bitch. that's fucking dumb as hell. 
+# Default to tabwidth = 2
+# relative line numbers
+# relative line numbers in explorer
+# grep thingie in folder
+# disable arrow keys
+# disable mouse (in all terminal instances and neovim etc...) ()
+# lsp easy
+# tree-sitter easy
+# auto indent detection
+# harpoon or fuzzy finder or file explorer or similar (pick one)
+# undo-tree
+# system clipboard
+# Auto format
+# Autosave on close
+# Auto sudo on write protected
+# haskell with good lsp. hlint builtin.
+# Se lsp and tree-sitter errors
+# Autocomplete
+# TODO notes highlighted
+# TODO look at kickstart nvim for inspiration
 }
--- system/default.nix
+++ system/default.nix
@@ -41,6 +41,7 @@
         # You can use https://search.nixos.org/ to find more packages (and options).
         environment.systemPackages = with pkgs; [
                 # nvcat # ^^
+		tree-sitter
                 prismlauncher
                 heroic
                 pavucontrol
