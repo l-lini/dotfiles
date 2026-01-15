faa64717adc901ed97e35582bcb8ed34232fbe00
Lini <Lini>
2026-01-15 11:20:08 +0100

    added prusa slicer

--- system/default.nix
+++ system/default.nix
@@ -41,6 +41,7 @@
         # You can use https://search.nixos.org/ to find more packages (and options).
         environment.systemPackages = with pkgs; [
                 # nvcat # ^^
+		prusa-slicer
 		acpid
 		tree-sitter
                 prismlauncher
