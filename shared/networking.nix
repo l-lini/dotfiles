c38ed63a9eea864093d04dc839795a00a15341c1
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-03 23:35:13 +0100

    eduroam config, probably works maybe. possibly... not tested

--- shared/networking.nix
+++ shared/networking.nix
@@ -32,6 +32,17 @@
             in {
                 Merkurius = createWifi "Merkurius";
                 Lini = createWifi "Lini";
+                eduroam = with createWifi "eduroam"; {
+                    inherit wifi connection ipv4 ipv6;
+                    wifi-security.key-mgmt = "wpa-eap";
+                    "802-1x" = {
+                        eap = "peap";
+                        domain-suffix-match = "eduroam.chalmers.se";
+                        identity = "alteborn@chalmers.se";
+                        password = secrets.eduroam;
+                        phase2-auth = "mschapv2";
+                    };
+                };
             };
         };
     };
