a910b7401bc35c945fc28efceaca099195f1577d
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-04 14:13:04 +0100

    move helper and fix wifi for monster

--- shared/networking.nix
+++ shared/networking.nix
@@ -1,35 +1,34 @@
 { hostName, secrets, ... }:
 
-{
+let
+createWifi = ssid: {
+    connection = {
+        id = ssid;
+        type = "wifi";
+    };
+    wifi = {
+        mode = "infrastructure";
+        inherit ssid;
+    };
+    wifi-security = {
+        auth-alg = "open";
+        key-mgmt = "wpa-psk";
+        psk = secrets.${ssid};
+    };
+    ipv4.method = "auto";
+    ipv6 = {
+        addr-gen-mode = "default";
+        method = "auto";
+    };
+};
+in {
 	networking = {
         firewall.enable = false;
         hostId = "e16f9e84"; # used by zfs
         hostName = hostName;
         networkmanager = {
             enable = true;
-            ensureProfiles.profiles = let
-                createWifi = ssid: {
-                    connection = {
-                        id = ssid;
-                        type = "wifi";
-                        interface-name = "wlp0s20f3";
-                    };
-                    wifi = {
-                        mode = "infrastructure";
-                        inherit ssid;
-                    };
-                    wifi-security = {
-                        auth-alg = "open";
-                        key-mgmt = "wpa-psk";
-                        psk = secrets.${ssid};
-                    };
-                    ipv4.method = "auto";
-                    ipv6 = {
-                        addr-gen-mode = "default";
-                        method = "auto";
-                    };
-                };
-            in {
+            ensureProfiles.profiles = {
                 Merkurius = createWifi "Merkurius";
                 Lini = createWifi "Lini";
                 eduroam = with createWifi "eduroam"; {
