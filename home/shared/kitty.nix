c8985b3973073f23c057464185a844a1838f8669
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-06 00:02:35 +0100

    format kitty.nix

--- home/shared/kitty.nix
+++ home/shared/kitty.nix
@@ -1,85 +1,85 @@
 { ... }:
 
 {
-    programs.kitty = {
-        enable = true;
-        settings = {
-            enable_audio_bell = false;
-            font_family = "Comic Mono";
-            font_style = "Bold";
-            font_size = 20.0;
-# clear_all_mouse_actions = "yes"; Disables mouse:
-            scrollback_pager = "nvim --cmd 'set eventignore=FileType' +'nnoremap q ZQ' +'call nvim_open_term(0, {})' +'set nomodified nolist' +'$' -";
+  programs.kitty = {
+    enable = true;
+    settings = {
+      enable_audio_bell = false;
+      font_family = "Comic Mono";
+      font_style = "Bold";
+      font_size = 20.0;
+      # clear_all_mouse_actions = "yes"; Disables mouse:
+      scrollback_pager = "nvim --cmd 'set eventignore=FileType' +'nnoremap q ZQ' +'call nvim_open_term(0, {})' +'set nomodified nolist' +'$' -";
 
-# Tokyonight colors
-# adapted from https://github.com/davidmathers/tokyo-night-kitty-theme
-            foreground = "#a9b1d6";
-            background = "#1a1b26";
+      # Tokyonight colors
+      # adapted from https://github.com/davidmathers/tokyo-night-kitty-theme
+      foreground = "#a9b1d6";
+      background = "#1a1b26";
 
-# Black
-            color0 = "#414868";
-            color8 = "#414868";
+      # Black
+      color0 = "#414868";
+      color8 = "#414868";
 
-# Red
-            color1 = "#f7768e";
-            color9 = "#f7768e";
+      # Red
+      color1 = "#f7768e";
+      color9 = "#f7768e";
 
-# Green
-            color2  = "#73daca";
-            color10 = "#73daca";
+      # Green
+      color2 = "#73daca";
+      color10 = "#73daca";
 
-# Yellow
-            color3  = "#e0af68";
-            color11 = "#e0af68";
+      # Yellow
+      color3 = "#e0af68";
+      color11 = "#e0af68";
 
-# Blue
-            color4  = "#7aa2f7";
-            color12 = "#7aa2f7";
+      # Blue
+      color4 = "#7aa2f7";
+      color12 = "#7aa2f7";
 
-# Magenta
-            color5  = "#bb9af7";
-            color13 = "#bb9af7";
+      # Magenta
+      color5 = "#bb9af7";
+      color13 = "#bb9af7";
 
-# Cyan
-            color6  = "#7dcfff";
-            color14 = "#7dcfff";
+      # Cyan
+      color6 = "#7dcfff";
+      color14 = "#7dcfff";
 
-# White
-            color7  = "#c0caf5";
-            color15 = "#c0caf5";
+      # White
+      color7 = "#c0caf5";
+      color15 = "#c0caf5";
 
-# Cursor
-            cursor = "#c0caf5";
-            cursor_text_color = "#1a1b26";
+      # Cursor
+      cursor = "#c0caf5";
+      cursor_text_color = "#1a1b26";
 
-# Selection highlight
-            selection_foreground = "none";
-            selection_background = "#28344a";
+      # Selection highlight
+      selection_foreground = "none";
+      selection_background = "#28344a";
 
-# The color for highlighting URLs on mouse-over
-            url_color = "#9ece6a";
+      # The color for highlighting URLs on mouse-over
+      url_color = "#9ece6a";
 
-# Window borders
-            active_border_color = "#3d59a1";
-            inactive_border_color = "#101014";
-            bell_border_color = "#e0af68";
+      # Window borders
+      active_border_color = "#3d59a1";
+      inactive_border_color = "#101014";
+      bell_border_color = "#e0af68";
 
-# Tab bar
-            tab_bar_style = "fade";
-            tab_fade = 1;
-            active_tab_foreground   = "#3d59a1";
-            active_tab_background   = "#16161e";
-            active_tab_font_style = "bold";
-            inactive_tab_foreground = "#787c99";
-            inactive_tab_background = "#16161e";
-            inactive_tab_font_style = "bold";
-            tab_bar_background = "#101014";
+      # Tab bar
+      tab_bar_style = "fade";
+      tab_fade = 1;
+      active_tab_foreground = "#3d59a1";
+      active_tab_background = "#16161e";
+      active_tab_font_style = "bold";
+      inactive_tab_foreground = "#787c99";
+      inactive_tab_background = "#16161e";
+      inactive_tab_font_style = "bold";
+      tab_bar_background = "#101014";
 
-# Title bar
-            macos_titlebar_color = "#16161e";
+      # Title bar
+      macos_titlebar_color = "#16161e";
 
-        };
     };
+  };
 }
 # Command history Search. Me forgor often );
 # Comic mono (better readability)
