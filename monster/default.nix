e81062f760e590dda690bc3f334e1280bbe57ac7
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-05 20:13:57 +0100

    spotifyd

--- monster/default.nix
+++ monster/default.nix
@@ -1,9 +1,18 @@
-{ ... }:
+{ secrets, ... }:
 
 {
-	imports = [
-		../shared
-        ./hardware.nix  # TODO
-        ./disko.nix     # TODO
-	];
+  imports = [
+    ../shared
+    ./hardware.nix # TODO
+    ./disko.nix # TODO
+  ];
+
+  services.spotifyd = {
+    enable = true;
+    settings.global = {
+      username = "linus_lego";
+      password = secrets.spotify;
+      device_name = "monster";
+    };
+  };
 }
