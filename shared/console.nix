e9ef2f33f29f89ab4496ba6eb3d03e7d80923e0d
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-06 00:02:12 +0100

    nice tty styling lol. Why bother? Good question

--- shared/console.nix
+++ shared/console.nix
@@ -1,8 +1,34 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
+  systemd.services.reload-systemd-vconsole-setup.serviceConfig.ExecStart = lib.mkForce (
+    pkgs.writeShellScript "reset-console" ''
+      until test -c /dev/dri/card1; do sleep 1; done
+      ${pkgs.systemd}/lib/systemd/systemd-vconsole-setup
+    ''
+  );
+
   console = {
     earlySetup = true;
-    font = "latarcyrheb-sun32"; # Enums, why are there no enums );
+    font = "latarcyrheb-sun32"; # Enums, why are they not enums );
+    colors = [
+      # TODO declare global colors for all modules to use
+      "1a1b26" # Black
+      "f7768e" # Red
+      "73daca" # Green
+      "30af68" # Yellow
+      "7112f7" # Blue
+      "bb9af7" # Magenta
+      "7dcfff" # Cyan
+      "c0caf5" # White
+      "1a1b26" # Black
+      "f7768e" # Red
+      "73daca" # Green
+      "30af68" # Yellow
+      "7112f7" # Blue
+      "bb9af7" # Magenta
+      "7dcfff" # Cyan
+      "c0caf5" # White
+    ];
   };
 }
