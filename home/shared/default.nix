caf15e821074799c08082fd3180892b7a3c246af
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-06 00:14:59 +0100

    add Todo

--- home/shared/default.nix
+++ home/shared/default.nix
@@ -1,14 +1,15 @@
 { inputs, ... }:
 
 {
-    imports = [
-        ./sway.nix
-            inputs.md307.homeModules.default
-# TODO Firefox, with extensions and stuffies. Ask Cal!
-# TODO bluetooth shortcut
-# TODO Comic sans and Comic mono fonts.
-    ];
+  imports = [
+    ./sway.nix
+    inputs.md307.homeModules.default
+    # TODO easy clip saving (my mic also included)
+    # TODO Firefox, with extensions and stuffies. Ask Cal!
+    # TODO bluetooth shortcut
+    # TODO Comic sans and Comic mono fonts.
+  ];
 
-    programs.zsh.initContent = "# Shutup bitch - John Cena";
-    home.stateVersion = "25.11";
+  programs.zsh.initContent = "# Shutup bitch - John Cena";
+  home.stateVersion = "25.11";
 }
