b0cde1874764340d4eba052503115e366bbdc4e1
l-lini <119787571+l-lini@users.noreply.github.com>
2026-02-05 20:14:51 +0100

    format and add default push

--- shared/git.nix
+++ shared/git.nix
@@ -1,20 +1,21 @@
 { pkgs, secrets, ... }:
 
 {
-        programs.git = {
-                enable = true;
-                config = {
-			init.defaultBranch = "main";
-			user = {
-                        	name = "l-lini";
-                        	email = "119787571+l-lini@users.noreply.github.com";
-                	};
-			credential.helper = "!${pkgs.writeShellScript "git-cred" ''
-				echo "username=l-lini"
-				echo "password=${secrets.github}"
-			''}";
-		};
-        };
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = "true";
+      user = {
+        name = "l-lini";
+        email = "119787571+l-lini@users.noreply.github.com";
+      };
+      credential.helper = "!${pkgs.writeShellScript "git-cred" ''
+        				echo "username=l-lini"
+        				echo "password=${secrets.github}"
+        			''}";
+    };
+  };
 }
 # Git default pull/push with current branch
 # Git default pull with rebase
