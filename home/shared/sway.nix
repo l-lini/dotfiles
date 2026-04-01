d7698e82b97184c7a97309008e5204359fa3be04
l-lini <119787571+l-lini@users.noreply.github.com>
2026-04-02 00:10:54 +0200

    cleanup

--- home/shared/sway.nix
+++ home/shared/sway.nix
@@ -1,4 +1,4 @@
-{ pencils, ... }:
+{ ... }:
 # Make stuff look nice: (Comic Font for readability) (Nice Colorscheme) (Simple)
 # TODO change mirror screen on button press is on nixos wiki for sway under tips and tricks.
 # TODO Keybind for activity bar
@@ -14,16 +14,12 @@
 # TODO! Make Sway nice
 #   TODO! (Nice Notifications)
 #   TODO! (Nice Background)
-#   TODO! (Borders etc)
-#   TODO! Fix AutoTiler (Master Slave layout)
-#   TODO! (Blurred translucent on unfocused)
-#   TODO! (Opaque on focused)
 # TODO! Make Browser nice
 # TODO! Low battery notification
 # TODO! Fix qutebrowser crashing
 # TODO! Fix qutebrowser not working with SEB, Ladok etc ...
 # TODO! Fix Volume Mute and Mic Mute Lights
-# TODO! Fix notification delay
+# TODO! Fix notification delay (send a notification on boot with a nice message! emojis and stuff!)
 # TODO! Replace Caps-Lock
 {
   imports = [
@@ -41,15 +37,6 @@
       notificationFromBash = notificationWithTime "4000";
       notificationWithTime =
         time: command: builtins.trace command "sh -c 'notify-send -t ${time} $(${command})'";
-      keys = {
-        modifier = "Mod4";
-        resize = "Ctrl";
-        move = "Shift";
-        left = "h";
-        down = "j";
-        up = "k";
-        right = "l";
-      };
     in
     {
       enable = true;
@@ -69,117 +56,43 @@
           }
         ];
         bars = [ ];
-        colors =
-          with pencils;
-          let
-            focused = {
-              background = black;
-              border = blue;
-              childBorder = bright.blue;
-              indicator = cyan;
-              text = white;
-            };
-
-            unfocused = {
-              background = bright.black;
-              border = bright.yellow;
-              childBorder = yellow;
-              indicator = bright.cyan;
-              text = white;
-            };
-
-            urgent = {
-              background = red;
-              border = red;
-              childBorder = red;
-              indicator = red;
-              text = black;
-            };
-
-            placeholder = urgent; # For restoring layouts
-            focusedInactive = urgent; # Placeholder mapping
-          in
-          {
-            background = black;
-            focusedInactive = focusedInactive;
-            placeholder = placeholder;
-            urgent = urgent;
-            unfocused = unfocused;
-            focused = focused;
-          };
         defaultWorkspace = "workspace number 1";
-        floating.border = 2;
-        floating.criteria = [ ]; # list of attributes to be floating
-        floating.modifier = keys.modifier;
-        floating.titlebar = false;
-        focus.followMouse = true;
-        focus.mouseWarping = true;
-        # focus.newWindow = "smart"; # smart, urgent, focus or none
+        floating = {
+          criteria = [ ];
+          modifier = "Mod4";
+          border = 0;
+          titlebar = false;
+        };
         input."type:touchpad" = {
           natural_scroll = "enabled";
           tap = "enabled";
         };
-        keybindings =
-          (with keys; {
-            "${modifier}+Return" = "exec kitty";
-            "${modifier}+Space" = "exec wofi --show run";
-            "${modifier}+b" = "exec qutebrowser";
-            "${modifier}+p" = "exec qutebrowser --target private-window";
-          })
-          // (with keys; {
-            "${modifier}+Backspace" = "kill";
-            "${modifier}+f" = "fullscreen toggle";
-            "${modifier}+${resize}+${left}" = "resize shrink width 100 px";
-            "${modifier}+${resize}+${right}" = "resize grow width 100 px";
-            "${modifier}+${resize}+${up}" = "resize grow height 100 px";
-            "${modifier}+${resize}+${down}" = "resize shrink height 100 px";
-            "${modifier}+${move}+${left}" = "move left";
-            "${modifier}+${move}+${right}" = "move right";
-            "${modifier}+${move}+${up}" = "move up";
-            "${modifier}+${move}+${down}" = "move down";
-            "${modifier}+${left}" = "focus left";
-            "${modifier}+${right}" = "focus right";
-            "${modifier}+${up}" = "focus up";
-            "${modifier}+${down}" = "focus down";
-            "${modifier}+w" = "exec ${notificationFromBash "workspace-status"}";
-            "${modifier}+1" = "workspace number 1";
-            "${modifier}+2" = "workspace number 2";
-            "${modifier}+3" = "workspace number 3";
-            "${modifier}+4" = "workspace number 4";
-            "${modifier}+5" = "workspace number 5";
-            "${modifier}+6" = "workspace number 6";
-            "${modifier}+7" = "workspace number 7";
-            "${modifier}+8" = "workspace number 8";
-            "${modifier}+9" = "workspace number 9";
-            "${modifier}+0" = "workspace number 10";
-            "${modifier}+${move}+1" = "move to workspace number 1";
-            "${modifier}+${move}+2" = "move to workspace number 2";
-            "${modifier}+${move}+3" = "move to workspace number 3";
-            "${modifier}+${move}+4" = "move to workspace number 4";
-            "${modifier}+${move}+5" = "move to workspace number 5";
-            "${modifier}+${move}+6" = "move to workspace number 6";
-            "${modifier}+${move}+7" = "move to workspace number 7";
-            "${modifier}+${move}+8" = "move to workspace number 8";
-            "${modifier}+${move}+9" = "move to workspace number 9";
-            "${modifier}+${move}+0" = "move to workspace number 10";
-            "${modifier}+t" = "exec ${notificationFromBash "date-status"}";
-            "${modifier}+v" = "exec ${notificationFromBash "sink-volume"}";
-          });
+        keybindings = ({
+          "Mod4+Return" = "exec kitty";
+          "Mod4+Space" = "exec wofi --show run";
+          "Mod4+b" = "exec qutebrowser";
+          "Mod4+p" = "exec qutebrowser --target private-window";
+          "Mod4+Backspace" = "kill";
+          #"Mod4+f" = "fullscreen toggle";
+          "Mod4+h" = "focus left";
+          "Mod4+l" = "focus right";
+          "Mod4+k" = "focus up";
+          "Mod4+j" = "focus down";
+          "Mod4+w" = "exec ${notificationFromBash "workspace-status"}";
+          "Mod4+1" = "workspace number 1";
+          "Mod4+2" = "workspace number 2";
+          "Mod4+3" = "workspace number 3";
+          "Mod4+Shift+1" = "move to workspace number 1";
+          "Mod4+Shift+2" = "move to workspace number 2";
+          "Mod4+Shift+3" = "move to workspace number 3";
+          "Mod4+t" = "exec ${notificationFromBash "date-status"}";
+          "Mod4+v" = "exec ${notificationFromBash "sink-volume"}";
+        });
         seat."*".hide_cursor = "when-typing enable";
         window = {
-          border = 5;
-          commands = [
-            {
-              command = "pkill steam";
-              criteria = {
-                class = "Steam";
-              };
-            } # lol
-          ];
-          hideEdgeBorders = "both";
+          border = 0;
           titlebar = false;
         };
-        # workspaceAutoBackAndForth = true;
       };
       xwayland = true;
     };
