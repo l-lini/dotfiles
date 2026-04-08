870f370f3781e3d26744961875e6bc95a77c354f
l-lini <119787571+l-lini@users.noreply.github.com>
2026-04-08 13:40:48 +0200

    ssh-server.nix

---
+++ nixos/ssh-server.nix
@@ -0,0 +1,39 @@
+{
+  port,
+  interface,
+  address,
+  defaultGateway ?
+    with builtins;
+    let
+      xs = splitVersion address;
+      x = elemAt xs;
+      xs' = map x (genList (x: x) 3) ++ [ "1" ];
+    in
+    concatStringsSep "." xs',
+  nameservers ? [
+    "1.1.1.1"
+  ],
+}:
+
+{
+  services.openssh = {
+    enable = true;
+    ports = [ port ];
+  };
+
+  networking.networking = {
+    inherit nameservers defaultGateway;
+
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ port ];
+    };
+
+    interfaces.${interface}.ipv4.addresses = [
+      {
+        address = address;
+        prefixLength = 24;
+      }
+    ];
+  };
+}
