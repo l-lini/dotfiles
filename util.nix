9acbeb6e2218e923bf2be41b436c51f200ed6dd5
l-lini <119787571+l-lini@users.noreply.github.com>
2026-04-16 21:20:03 +0200

    util things

--- util.nix
+++ util.nix
@@ -1,41 +1,37 @@
 with builtins;
 rec {
   inherit concatStringSep;
+  last = xs: elemAt xs (length xs - 1);
+  tryElemAt = i: xs: if length xs > i then elemAt xs i else null;
+  orElse = x: nullable: if nullable == null then x else nullable;
+  extension = baseName: (head (orElse [ "" ] (tryElemAt 1 (split ".*([.].*)" baseName))));
+  pathToName =
+    path:
+    let
+      baseName = baseNameOf path;
+      ext = extension baseName;
+    in
+    substring 0 (stringLength baseName - stringLength ext) baseName;
   pathsInDir = dir: map (name: /${dir}/${name}) (attrNames (readDir dir));
   dirToAttr =
     dir: nameF: valueF:
     listToAttrs (
       map (path: {
         name = nameF path;
-        value = valueF path;
+        value = valueF (nameF path);
       }) (pathsInDir dir)
     );
-  readDirFiles = dir: dirToAttr dir baseNameOf readFile;
-  endsWith =
-    end: s:
-    let
-      s' = toString s;
-      le = stringLength end;
-      ls = stringLength s';
-    in
-    substring (ls - le) le s' == end;
-  removeEnd =
-    end: s:
-    let
-      s' = toString s;
-      le = stringLength end;
-      ls = stringLength s';
-    in
-    if endsWith end s' then substring 0 (ls - le) s' else s';
-  nameOfFile = end: path: removeEnd end (baseNameOf path);
-  # !BEWARE OF FUNKY REGEX SHIT!
-  splitString = sep: s: filter isString (split (toString sep) (toString s));
+  hexToUpper = replaceStrings [ "a" "b" "c" "d" "e" "f" ] [ "A" "B" "C" "D" "E" "F" ];
   pencil = {
     void = "000000";
     ground = "002233";
     text = "ffffff";
     bad = "ff0000";
     good = "00ff00";
-    keyword = "ff9966";
+    keyword = "eeaa66";
+    variable = "ff0077";
+    function = "00bbdd";
+    literal = "99cc66";
+    comment = "334477";
   };
 }
