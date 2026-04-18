6c1ba29313ab5952ecba95b4f93d352bdab567da
l-lini <119787571+l-lini@users.noreply.github.com>
2026-04-18 23:57:08 +0200

    add todo

--- os/zsh.nix
+++ os/zsh.nix
@@ -4,6 +4,7 @@
 # command history thingy with fuzzy finding.
 # Alias for nixupdate = git add . && git commit -m "$1" && git push && sudo nixos-rebuild switch
 # Alias for nixtest = git add . && sudo nixos-rebuild test
+# Remove .zshrc prompt at boot
 { ... }:
 {
   programs.zsh = {
