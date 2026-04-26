809b04bc2713f4850d086717719608daf424c531
l-lini <119787571+l-lini@users.noreply.github.com>
2026-04-26 22:33:29 +0200

    batter notif idk if working yet

--- os/battery.nix
+++ os/battery.nix
@@ -1,5 +1,28 @@
-{ ... }:
-# TODO battery notification daemon
+{ pkgs, ... }:
+
 {
   environment.shellAliases.batteri = "echo \"$(cat /sys/class/power_supply/BAT0/capacity) $(cat /sys/class/power_supply/BAT0/status)\"";
+
+  systemd.user = {
+    services.low-battery = {
+      enable = true;
+      partOf = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = pkgs.writeShellScript "low-battery" ''
+          if (( 27 >= $(cat /sys/class/power_supply/BAT0/capacity)));
+          then ${pkgs.lib.getExe pkgs.pkgs.libnotify} --urgency=critical "$(batteri)";
+          fi;
+        '';
+      };
+    };
+    timers.low-battery = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-* *:*:00";
+        Unit = "low-battery.service";
+      };
+    };
+  };
 }
