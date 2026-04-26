2f49e82dbc3df4d38bef8661381cebec050025e3
l-lini <119787571+l-lini@users.noreply.github.com>
2026-04-26 17:56:49 +0200

    add ssh host

--- os/ssh-client.nix
+++ os/ssh-client.nix
@@ -1,5 +1,13 @@
 { ... }:
 
 {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+  };
+  programs.ssh.extraConfig = "
+      Host bull
+        Hostname alteborn.se
+        Port 42069
+        User lini
+    ";
 }
