a6d7438df423c640c4cc4f81f43bfd31861c47da
l-lini <119787571+l-lini@users.noreply.github.com>
2026-08-19 12:11:05 +0200

    annoying docker thing

--- os/docker.nix
+++ os/docker.nix
@@ -3,4 +3,8 @@
 {
   users.users.${username}.extraGroups = [ "docker" ];
   virtualisation.docker.enable = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "docker-28.5.2"
+    "electron-39.8.10"
+  ];
 }
